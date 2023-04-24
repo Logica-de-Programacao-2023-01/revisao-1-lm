4118af6d2284486d91ad021ac3f3cb2c3115e87f
Lucas Porto <69583259+hayyatty@users.noreply.github.com>
2023-04-24 14:22:43 -0300

    Add files via upload

--- q1/q1.go
+++ q1/q1.go
@@ -1,6 +1,50 @@
 package q1
 
+import "fmt"
+
 func CalculateDiscount(currentPurchase float64, purchaseHistory []float64) (float64, error) {
 	// Implemente sua solução aqui
-	return 0, nil
+	var (
+		mediadecompras   float64
+		desc             float64
+		histdecompras    float64
+		descontodacompra float64
+	)
+	for i := 0; i < len(purchaseHistory); i++ {
+		compra := purchaseHistory[i]
+		histdecompras += compra
+		mediadecompras = histdecompras / float64(len(purchaseHistory))
+	}
+	if mediadecompras > 1000 {
+		desc = 0.2
+	} else if histdecompras > 1000 {
+		desc = 0.1
+	} else if histdecompras == 0 {
+		desc = 0.1
+	} else if histdecompras <= 500 {
+		desc = 0.02
+	} else if histdecompras <= 1000 {
+		desc = 0.05
+	}
+
+	if currentPurchase == 0 {
+		return 0, fmt.Errorf("O valor da compra atual é 0")
+	} else {
+		descontodacompra = currentPurchase * desc
+		return descontodacompra, nil
+	}
 }
+
+/* Uma loja de roupas precisa de uma função para verificar se um cliente é elegível para um desconto com base em seu histórico de compras.
+A função deve receber o valor da compra atual e o histórico de compras do cliente. A função deve retornar o valor do desconto e um erro.
+O erro deve ser nulo (ou seja, nil) se não houver nenhum erro. Caso o valor da compra atual seja menor ou igual a zero,
+a função deve retornar um erro com a mensagem "valor da compra inválido".
+
+Regras de desconto:
+
+Se o valor total de compras do cliente for maior que R$ 1000,00, o desconto é de 10% do valor da compra atual.
+Se o valor total de compras do cliente for menor ou igual a R$ 1000,00, o desconto é de 5% do valor da compra atual.
+Se o valor total de compras do cliente for menor ou igual a R$ 500,00, o desconto é de 2% do valor da compra atual.
+Se a compra atual for a primeira do cliente, o desconto é de 10% do valor da compra atual.
+Se a média de compras do cliente for maior que R$ 1000,00, o desconto é de 20% do valor da compra atual.
+Os descontos não são cumulativos. Ou seja, se o cliente tiver direito a mais de um desconto, deve ser aplicado o maior desconto.*/
--- q4/q4.go
+++ q4/q4.go
@@ -1,6 +1,69 @@
 package q4
 
+import "fmt"
+
 func CalculateFinalPrice(basePrice float64, state string, taxCode int) (float64, error) {
 	// Implemente sua solução aqui
-	return 0, nil
+	var (
+		FreteEstado float64
+		aliquota    float64
+		pfinal      float64
+	)
+	if taxCode != 1 && taxCode != 2 && taxCode != 3 {
+		return 0, fmt.Errorf("imposto não encontrado")
+	} else if basePrice <= 0 {
+		return 0, fmt.Errorf("preço base inválido")
+	}
+	if state == "SP" {
+		/*10%*/
+		FreteEstado = 0.1
+	} else if state == "RJ" {
+		/*15%*/
+		FreteEstado = 0.15
+	} else if state == "MG" {
+		/*20%*/
+		FreteEstado = 0.2
+	} else if state == "ES" {
+		/*25%*/
+		FreteEstado = 0.25
+	} else {
+		/*30%*/
+		FreteEstado = 0.3
+	}
+	switch taxCode {
+	case 1:
+		/*5%*/
+		aliquota = 0.05
+	case 2:
+		/*10%*/
+		aliquota = 0.1
+	case 3:
+		/*15%*/
+		aliquota = 0.15
+	}
+
+	pfinal = basePrice + ((basePrice * aliquota) + (basePrice * FreteEstado))
+
+	return pfinal, nil
 }
+
+/* Uma empresa de comércio eletrônico precisa de uma função para calcular o preço final de um produto, que inclui o preço base, impostos e frete.
+A função deve receber o preço base, o estado de destino e o código do imposto.
+
+A alíquota do imposto e o percentual de frete se baseiam nas tabelas abaixo:
+
+Código do imposto	Alíquota
+1	5%
+2	10%
+3	15%
+Estado de destino	Percentual de frete
+SP	10%
+RJ	15%
+MG	20%
+ES	25%
+OUTROS	30%
+
+A função deve retornar o preço final e um erro. O erro deve ser nulo (ou seja, nil) se não houver nenhum erro.
+Caso o código do imposto não exista, a função deve retornar um erro com a mensagem "imposto não encontrado".
+Caso o preço base seja menor ou igual a zero, a função deve retornar um erro com a mensagem "preço base inválido".
+*/
